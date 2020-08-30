567275a8627378e8d3bb21370e1163e44226a538
fdavison1 <fred.noreply@gmail.com>
2020-08-30 14:47:21 -0500

    filter reduce sort

---
+++ src/Container.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const fakeData = [
+    {
+        "month": "aug",
+        "amount": "-32.09",
+        "cat1": "other",
+        "cat2": "test",
+    },
+    {
+        "month": "aug",
+        "amount": "-132.09",
+        "cat1": "other",
+        "cat2": "something",
+    },
+    {
+        "month": "aug",
+        "amount": "-12.09",
+        "cat1": "other",
+        "cat2": "shopping",
+    },
+    {
+        "month": "aug",
+        "amount": "-100",
+        "cat1": "other",
+        "cat2": "shopping",
+    },
+    {
+        "month": "aug",
+        "amount": "-32.09",
+        "cat1": "something",
+        "cat2": "shopping",
+    }
+]
+
+const currentMonth = 'aug';
+const newRows = [];
+
+const filterRows = fakeData.filter(row => row.month === currentMonth && row.cat1 === 'other');
+const reduceRows = filterRows.reduce((acc, row) => {
+
+    const match = newRows.find(newRow => (newRow.cat2 === row.cat2))
+
+    if (match){
+        const updatedRow = {
+            ...match,
+            amount: (parseFloat(match.amount) + parseFloat(row.amount)).toFixed(2)
+        }
+        newRows.pop(match)
+        newRows.splice(0, 0, updatedRow)
+        newRows.sort((a,b) => a.amount - b.amount)
+
+    } else {
+        newRows.push(row)
+    }
+    return newRows
+}, 0)
+
+export const Container = () => {
+    return (
+        <div>
+            <h1>Other Spending - August</h1>
+            <table>
+                <tbody>
+                {reduceRows.map((row, i) => {
+                return (<tr key={i}>
+                    <td>{row.month}</td>
+                    <td>{row.date}</td>
+                    <td className='right'>{-row.amount}</td>
+                    <td>{row.cat1}</td>
+                    <td>{row.cat2}</td>
+                    <td>{row.description}</td>
+                </tr>)
+            })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
