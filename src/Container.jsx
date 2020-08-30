731878d4773b5ca2dbfdb954a3e733c51d6f15eb
fdavison1 <fred.noreply@gmail.com>
2020-08-30 17:05:43 -0500

    bills section

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,45 +1,66 @@
 import React from 'react';
-import {fakeData} from './constants/amounts';
+import { fakeData } from './constants/amounts';
 
 const currentMonth = 'aug';
-const newRows = [];
 
-const filterRows = fakeData.filter(row => row.month === currentMonth && row.cat1 === 'other');
-const reduceRows = filterRows.reduce((acc, row) => {
-    
-    const match = newRows.find(newRow => (newRow.cat2 === row.cat2))
+const reduceRows = category => {
+    const newRows = [];
 
-    if (match) {
-        const updatedRow = {
-            ...match,
-            amount: (parseFloat(match.amount) + parseFloat(row.amount)).toFixed(2)
+    const filterRows = fakeData.filter(row => row.month === currentMonth && row.cat1 === category);
+    filterRows.reduce((acc, row) => {
+
+        const match = newRows.find(newRow => (newRow.cat2 === row.cat2))
+        const matchIndex = newRows.findIndex(newRow => (newRow.cat2 === row.cat2))
+
+        if (match) {
+            const updatedRow = {
+                ...match,
+                amount: (parseFloat(match.amount) + parseFloat(row.amount)).toFixed(2)
+            }
+            newRows.splice(matchIndex, 1, updatedRow)
+
+        } else {
+            newRows.push(row)
         }
-        newRows.splice(newRows.findIndex(row => row.cat2 === match.cat2), 1, updatedRow)
-
-    } else {
-        newRows.push(row)
-    }
-
+        return null;
+    }, 0)
     newRows.sort((a, b) => a.amount - b.amount)
     return newRows
-}, 0)
+}
 
 export const Container = () => {
     return (
-        <div>
-            <h1>Other Spending - August</h1>
-            <table>
-                <tbody>
-                    {reduceRows.map((row, i) => {
-                        return (
-                            <tr key={i}>
-                                <td className='right'>{(-parseFloat(row.amount)).toFixed(2)}</td>
-                                <td>{row.cat2}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+        <div className='container'>
+            <div>
+                <h1>other</h1>
+                <table>
+                    <tbody>
+                        {reduceRows('other').map((row, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td className='right'>{(-parseFloat(row.amount)).toFixed(2)}</td>
+                                    <td>{row.cat2}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <h1>bills</h1>
+                <table>
+                    <tbody>
+                        {reduceRows('bills').map((row, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td className='right'>{(-parseFloat(row.amount)).toFixed(2)}</td>
+                                    <td>{row.cat2}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
