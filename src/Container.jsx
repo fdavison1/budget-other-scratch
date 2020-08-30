81581495543d8699d45bebe299bc8fb3968ae372
fdavison1 <fred.noreply@gmail.com>
2020-08-30 15:44:41 -0500

    export constant

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,37 +1,5 @@
 import React from 'react';
-
-const fakeData = [
-    {
-        "month": "aug",
-        "amount": "-32.09",
-        "cat1": "other",
-        "cat2": "test",
-    },
-    {
-        "month": "aug",
-        "amount": "-132.09",
-        "cat1": "other",
-        "cat2": "something",
-    },
-    {
-        "month": "aug",
-        "amount": "-12.09",
-        "cat1": "other",
-        "cat2": "shopping",
-    },
-    {
-        "month": "aug",
-        "amount": "-100",
-        "cat1": "other",
-        "cat2": "shopping",
-    },
-    {
-        "month": "aug",
-        "amount": "-32.09",
-        "cat1": "something",
-        "cat2": "shopping",
-    }
-]
+import {fakeData} from './constants/amounts';
 
 const currentMonth = 'aug';
 const newRows = [];
---
+++ src/constants/amounts.js
@@ -0,0 +1,32 @@
+export const fakeData = [
+    {
+        "month": "aug",
+        "amount": "-32.09",
+        "cat1": "other",
+        "cat2": "test",
+    },
+    {
+        "month": "aug",
+        "amount": "-132.09",
+        "cat1": "other",
+        "cat2": "something",
+    },
+    {
+        "month": "aug",
+        "amount": "-12.09",
+        "cat1": "other",
+        "cat2": "shopping",
+    },
+    {
+        "month": "aug",
+        "amount": "-100",
+        "cat1": "other",
+        "cat2": "shopping",
+    },
+    {
+        "month": "aug",
+        "amount": "-32.09",
+        "cat1": "something",
+        "cat2": "shopping",
+    }
+]
