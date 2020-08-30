075c8f91e46a80e6c9e2aa5c0df8447496f3fbd2
fdavison1 <fred.noreply@gmail.com>
2020-08-30 14:56:01 -0500

    alt rows

--- src/Container.jsx
+++ src/Container.jsx
@@ -41,14 +41,14 @@ const reduceRows = filterRows.reduce((acc, row) => {
 
     const match = newRows.find(newRow => (newRow.cat2 === row.cat2))
 
-    if (match){
+    if (match) {
         const updatedRow = {
             ...match,
             amount: (parseFloat(match.amount) + parseFloat(row.amount)).toFixed(2)
         }
         newRows.pop(match)
         newRows.splice(0, 0, updatedRow)
-        newRows.sort((a,b) => a.amount - b.amount)
+        newRows.sort((a, b) => a.amount - b.amount)
 
     } else {
         newRows.push(row)
@@ -62,16 +62,14 @@ export const Container = () => {
             <h1>Other Spending - August</h1>
             <table>
                 <tbody>
-                {reduceRows.map((row, i) => {
-                return (<tr key={i}>
-                    <td>{row.month}</td>
-                    <td>{row.date}</td>
-                    <td className='right'>{-row.amount}</td>
-                    <td>{row.cat1}</td>
-                    <td>{row.cat2}</td>
-                    <td>{row.description}</td>
-                </tr>)
-            })}
+                    {reduceRows.map((row, i) => {
+                        return (<tr key={i}>
+                            <td>{row.month}</td>
+                            <td className='right'>{-row.amount}</td>
+                            <td>{row.cat1}</td>
+                            <td>{row.cat2}</td>
+                        </tr>)
+                    })}
                 </tbody>
             </table>
         </div>
