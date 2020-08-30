1fa9ec02e68368bcfaf301783ab472b7769f451b
fdavison1 <fred.noreply@gmail.com>
2020-08-30 16:31:22 -0500

    fix splice

--- src/Container.jsx
+++ src/Container.jsx
@@ -6,7 +6,7 @@ const newRows = [];
 
 const filterRows = fakeData.filter(row => row.month === currentMonth && row.cat1 === 'other');
 const reduceRows = filterRows.reduce((acc, row) => {
-
+    
     const match = newRows.find(newRow => (newRow.cat2 === row.cat2))
 
     if (match) {
@@ -14,13 +14,13 @@ const reduceRows = filterRows.reduce((acc, row) => {
             ...match,
             amount: (parseFloat(match.amount) + parseFloat(row.amount)).toFixed(2)
         }
-        newRows.pop(match)
-        newRows.splice(0, 0, updatedRow)
-        newRows.sort((a, b) => a.amount - b.amount)
+        newRows.splice(newRows.findIndex(row => row.cat2 === match.cat2), 1, updatedRow)
 
     } else {
         newRows.push(row)
     }
+
+    newRows.sort((a, b) => a.amount - b.amount)
     return newRows
 }, 0)
 
@@ -33,8 +33,8 @@ export const Container = () => {
                     {reduceRows.map((row, i) => {
                         return (
                             <tr key={i}>
+                                <td className='right'>{(-parseFloat(row.amount)).toFixed(2)}</td>
                                 <td>{row.cat2}</td>
-                                <td className='right'>{-row.amount}</td>
                             </tr>
                         )
                     })}
